0cbbca56e4090c152a1c93b32dbad08bec8cb3d2
Karl Fryzek <63215849+Fryflex@users.noreply.github.com>
2022-02-09 19:17:12 -0500

    update1

--- src/main.c
+++ src/main.c
@@ -1,39 +1,31 @@
 #include <genesis.h>
 
 int main(u16 hard)
-void myJoyHandler( u16 joy, u16 changed, u16 state)
 {
-    if (joy == JOY_1)
-    {
-        if (state & BUTTON_START)
-        {
-            //player 1 press start button
-        }
-        else if (changed & BUTTON_START)
-        {
-            //player 1 released start button
-        }
+    int i = 1;
+    char vtext[20];
+    int vx = 1;
 
-        }
-    }
-}
-{
-    JOY_init();
-    JOY_setEventHandler( &myJoyHandler );
-    VDP_drawText("Hello Genny World!", 10, 13);
-    waitMs(1000);
-    VDP_resetScreen();
-    waitMs(750);
-    VDP_drawText("Hello Genesis World!!", 13, 10);
-    while(TRUE)
-    {
+    while(i <= 10){
+        i = i + 1;
+        SYS_doVBlankProcess();
+        VDP_drawText("Hello Genny World!", vx, 10);
+        waitMs(20);
+        VDP_resetScreen();
+        VDP_drawText("Hello Genesis World!!", 15, 5);
+        waitMs(20);
+        VDP_resetScreen();
+        sprintf(vtext, "%i", i);
+        VDP_drawText(vtext , 10 , 10);
+        waitMs(20);
+        VDP_resetScreen();
+        vx = vx +1;
         // read input
         // move sprite
         // update score
         // draw current screen (logo, start screen, settings, game, gameover, credits...)
 
         // wait for screen refresh
-        SYS_doVBlankProcess();
 
     }
 
